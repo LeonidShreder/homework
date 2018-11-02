a5a72cf0b5c64f09980b01f5fa81121ea6c7b304
leo <leonidshreder@gmail.com>
2018-11-02 17:51:53 +0200

    init

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+
 app.config.from_object(Config)
+app.config.from_pygile('config.py')
+
+db = SQLAlchemy(app)
